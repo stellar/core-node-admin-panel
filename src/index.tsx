b33e6c16661278d5a0b005344a1e510037c38a41
Michael Feldstein <msfeldstein@gmail.com>
2019-06-11 17:34:08 -0700

    Add prettier as a pre-commit hook, and run it on the entire src directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { StoreProvider } from './Store'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { StoreProvider } from "./Store";
 
-ReactDOM.render(<StoreProvider>
+ReactDOM.render(
+  <StoreProvider>
     <App />
-</StoreProvider>,
-document.getElementById('root'));
+  </StoreProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
