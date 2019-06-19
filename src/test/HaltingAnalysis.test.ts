c109180de3ebe8c657aa065c1cd601a82f40acda
Michael Feldstein <msfeldstein@gmail.com>
2019-06-19 16:39:14 -0700

    Better naming in tests

--- src/test/HaltingAnalysis.test.ts
+++ src/test/HaltingAnalysis.test.ts
@@ -4,14 +4,14 @@ import highlyDependent from "./data/HighlyDependent";
 
 describe("halting analysis", () => {
   it("must return an empty set for a healthy quorum", () => {
-    const dangerousNodes = haltingAnalysis(healthy, 1);
-    expect(dangerousNodes).toHaveLength(0);
+    const failureCases = haltingAnalysis(healthy, 1);
+    expect(failureCases).toHaveLength(0);
   });
 
   it("must return a node that is too highly depended on", () => {
-    const dangerousNodes = haltingAnalysis(highlyDependent, 1);
-    expect(dangerousNodes).toHaveLength(1);
-    expect(dangerousNodes[0].vulnerableNodes[0]).toHaveProperty("node", "e");
+    const failureCases = haltingAnalysis(highlyDependent, 1);
+    expect(failureCases).toHaveLength(1);
+    expect(failureCases[0].vulnerableNodes[0]).toHaveProperty("node", "e");
   });
 });
 
