613b9c357dee0e91598969950eca0be3b9ba7700
Michael Feldstein <msfeldstein@gmail.com>
2019-06-20 11:02:46 -0700

    Remove code that got into wrong branch

--- src/test/HaltingAnalysis.test.ts
+++ src/test/HaltingAnalysis.test.ts
@@ -1,7 +1,6 @@
 import { haltingAnalysis } from "../util/HaltingAnalysis";
 import healthy from "./data/HealthyQuorum";
 import highlyDependent from "./data/HighlyDependent";
-import cyclical from "./data/CyclicalUnhealthy";
 
 describe("halting analysis", () => {
   it("must return an empty set for a healthy quorum", () => {
@@ -13,11 +12,6 @@ describe("halting analysis", () => {
     const failureCases = haltingAnalysis(highlyDependent, 1);
     expect(failureCases).toHaveLength(1);
     expect(failureCases[0].vulnerableNodes[0]).toHaveProperty("node", "e");
-  });
-
-  it("must not infinite loop on cycles", () => {
-    const failureCases = haltingAnalysis(cyclical);
-    expect(failureCases).toHaveLength(2);
   });
 });
 
