d7df5d2580ae290d89866b34241ebf4f28eb6ea5
Michael Feldstein <msfeldstein@gmail.com>
2019-06-27 11:23:37 -0700

    Fix warning

--- src/Components/InfoPanel.tsx
+++ src/Components/InfoPanel.tsx
@@ -7,7 +7,6 @@ const InfoPanel: FunctionComponent = props => {
   const [showDetails, setShowDetails] = useState(true);
   let button = showDetails ? <Minus /> : <Plus />;
   let openClass = showDetails ? s.open : null;
-  let body = showDetails ? <>{props.children}</> : <div></div>;
   return (
     <div className={`${s.FailureDialog} ${openClass}`}>
       <div
